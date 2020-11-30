62d509b8c8ad5de22d83903cc7e1c5d8a0a9051b
Alex Marshall <alex.k.marshall83@gmail.com>
2020-11-30 17:17:02 +0000

    Add login/logout

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Factions from "./components/Factions";
+import LoginButton from "./components/Login";
+import LogoutButton from "./components/Logout";
+import Profile from "./components/Profile";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <LoginButton />
+      <LogoutButton />
+      <Profile />
+      <Factions />
+    </>
   );
 }
 
---
+++ src/components/Factions.tsx
@@ -0,0 +1,3 @@
+export default function Factions() {
+  return <div>Faction List</div>;
+}
