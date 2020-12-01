7a4c71af477e33de9a50a9e271946762704b7410
Alex Marshall <alex.k.marshall83@gmail.com>
2020-12-01 08:26:29 +0000

    Extract useAuthClient hook

---
+++ src/hooks/client.ts
@@ -0,0 +1,44 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import { useCallback } from "react";
+
+const apiUrl = "http://localhost:8000/api";
+
+export async function client(endpoint: string, config?: RequestInit) {
+  const response = await window.fetch(`${apiUrl}/${endpoint}`, config);
+  if (response.status === 401) {
+    return Promise.reject({ message: "Please re-authenticate" });
+  }
+  try {
+    const data = await response.json();
+    if (response.ok) {
+      return data;
+    } else {
+      return Promise.reject(data);
+    }
+  } catch (error) {
+    return Promise.reject(error);
+  }
+}
+
+export function useAuthClient() {
+  const { getAccessTokenSilently, isAuthenticated } = useAuth0();
+
+  return useCallback(
+    async (endpoint: string, config: RequestInit = {}) => {
+      const accessToken = isAuthenticated
+        ? await getAccessTokenSilently()
+        : undefined;
+      if (accessToken) {
+        config = {
+          ...config,
+          headers: {
+            ...config.headers,
+            Authorization: `Bearer ${accessToken}`,
+          },
+        };
+      }
+      return client(endpoint, config);
+    },
+    [getAccessTokenSilently, isAuthenticated]
+  );
+}
